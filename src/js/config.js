6c8d844364b15eca2eb42631c269ea11b83338ba
rold97 <dima.paraschuk@gmail.com>
2023-04-06 19:56:28 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '06d4eb3d-53c1-4879-b3c7-9de98768dc7e';
+export const MODAL_CLOSE_SEC = 1.5;
